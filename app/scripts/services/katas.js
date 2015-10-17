82d0ddfa52a60d1380ec1d6ddf6acdbf8699c7ee
Sebastian Thiel <byronimo@gmail.com>
2015-10-17 20:22:45 +0200

    feat(katas): text-based filtering and improved looks

--- app/scripts/services/katas.js
+++ app/scripts/services/katas.js
@@ -3,8 +3,26 @@
 angular.module('katarsApp')
   .factory('Katas',
     function() {
+
+      var difficulties = ['easy', 'medium', 'hard'];
+
       var katas = [{
         name: 'Kata1',
+        difficulty: difficulties[0],
+        short_description: "This one is easy",
+        description: "multilines ... or from url ...",
+      },
+      {
+        name: 'Kata 2',
+        difficulty: difficulties[1],
+        short_description: "This one is not so easy",
+        description: "multilines ... or from url ...",
+      },
+      {
+        name: 'Kata 3',
+        difficulty: difficulties[2],
+        short_description: "This one is quite hard",
+        description: "multilines ... or from url ...",
       }, ];
 
       return {
