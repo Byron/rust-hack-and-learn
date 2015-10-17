c8431de57a16da24f358a38b511fa0cb0bce0ede
Sebastian Thiel <byronimo@gmail.com>
2015-10-17 13:35:40 +0200

    chore(init): initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name katarsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the katarsApp
+ */
+angular.module('katarsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
