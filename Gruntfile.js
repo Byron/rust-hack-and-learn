7b6d1bb793869bb38ee5b157a5f7d38de35ab256
Sebastian Thiel <byronimo@gmail.com>
2015-10-25 13:31:19 +0100

    fix(grunt): cdnify requires a network connection, so it's disabled now

--- Gruntfile.js
+++ Gruntfile.js
@@ -491,7 +491,7 @@ module.exports = function (grunt) {
       'concat',
       'ngAnnotate',
       'copy:dist',
-      'cdnify',
+      // 'cdnify',
       'cssmin',
       'uglify',
       'filerev',
