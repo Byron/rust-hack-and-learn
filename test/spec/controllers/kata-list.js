ace85bd995e3c2fd107e80402efc4eed7746a638
Sebastian Thiel <byronimo@gmail.com>
2015-10-25 14:35:35 +0100

    fix(ctrl): fix tests for KataListCtrl

--- test/spec/controllers/kata-list.js
+++ test/spec/controllers/kata-list.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe('Controller: MainCtrl', function () {
+describe('Controller: KataListCtrl', function () {
 
   // load the controller's module
   beforeEach(module('katarsApp'));
@@ -11,7 +11,7 @@ describe('Controller: MainCtrl', function () {
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
+    MainCtrl = $controller('KataListCtrl', {
       $scope: scope
       // place here mocked dependencies
     });
